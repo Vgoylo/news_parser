bfe30587ba45c660e903ce6194e222fd74584923
vgoylo <y_draiv.by@mail.ru>
2021-09-22 09:45:38 +0300

    Got a picture

--- run.rb
+++ run.rb
@@ -3,6 +3,19 @@ require 'pry'
 
 mechanize = Mechanize.new
 
+#puts 'Enter onliner channen link'
+
+#input = gets.chomp
+
 page = mechanize.get('https://www.onliner.by/')
-binding.pry
-puts page.title
+links = page.links
+sample_link = links.sample
+
+sab_page = mechanize.get(sample_link.href)
+images = sab_page.images
+
+sample_image = images.sample
+
+
+p sample_image.url.display
+
